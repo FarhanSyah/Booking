63a3fa2cce3d9ae3ed3684791e1e62c25a13fb01
farhansyah <farhan_syah99@yahoo.com>
2016-09-20 18:43:32 +0700

    Changed Calendar API

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,8 +1,8 @@
-angular.module('starter.controllers', ['ion-datetime-picker'])
+angular.module('starter.controllers', ['ionic-datepicker'])
 .controller('DashCtrl', function($scope ) {
 
-    // $scope.venues = [{name: 'one', age: 30 },{ name: 'two', age: 27 },{ name: 'three', age: 50 }];
-    // $scope.selectedItem = $scope.venues[0];
+
+
     $scope.swiper = {};
  
     $scope.onReadySwiper = function (swiper) {
@@ -17,4 +17,25 @@ angular.module('starter.controllers', ['ion-datetime-picker'])
     };
 })
 .controller('AccountCtrl', function($scope ) {})
-.controller('SearchVenuesCtrl', function($scope ) {})
+.controller('SearchVenuesCtrl', function($scope, ionicDatePicker) {
+    $scope.venues = [{name: 'one', age: 30 },{ name: 'two', age: 27 },{ name: 'three', age: 50 }];
+    // $scope.selectedItem = $scope.venues[0];
+    $scope.dateval = {};
+    var ipObj1 = {
+      callback: function (val) {  //Mandatory
+        $scope.dateval.text = new Date(val);
+        console.log('Return value from the datepicker popup is : ' + val, new Date(val));
+      },
+      from: new Date(), //Optional
+      to: new Date(2050, 10, 30), //Optional
+      inputDate: new Date(),      //Optional
+      mondayFirst: true,          //Optional
+      closeOnSelect: true,       //Optional
+      templateType: 'modal',       //Optional
+      dateFormat: 'dd-MM-yyyy' //Optional
+    };
+    $scope.openDatePicker = function(){
+      ionicDatePicker.openDatePicker(ipObj1);
+    };
+
+})
